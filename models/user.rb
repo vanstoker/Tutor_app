418411ab852b8c67a220dfe211d7d1cdc02525b2
vanstoker <vanstoker@mail.ru>
2021-07-23 01:04:30 +0300

    Broken views

---
+++ models/user.rb
@@ -0,0 +1,33 @@
+class User < Sequel::Model
+  attr_accessor :password, :password_confirmation
+
+  def validate
+    super
+    validates_presence :password
+    validates_length_range 4..40, :password
+    validates_presence :password_confirmation
+    errors.add(:password_confirmation, "must confirm password") if password != password_confirmation
+  end
+
+  def before_save
+    super
+    encrypt_password
+  end
+
+private
+
+  def encrypt_password
+    self.password_hash = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = filter(Sequel.function(:lower, :email) => Sequel.function(:lower, email)).first
+   user && user.has_password?(password) ? user : nil
+  end
+
+  def has_password?(password)
+    BCrypt::Password.new(self.password_hash) == password
+  end
+
+  private
+end
--- myapp.rb
+++ myapp.rb
@@ -1,13 +1,23 @@
 require "roda"
 require "sequel"
+require "bcrypt"
+require "rack/protection"
 
 database = "myapp_development"
-user = ENV["PGUSER"]
-password = ENV["PGPASSWORD"]
+user = "postgres"
+password = "point"
 DB = Sequel.connect(adapter: "postgres", database: database, host: "127.0.0.1",
 										user: user, password: password)
 
 class Myapp < Roda
+	Sequel::Model.plugin :validation_helpers
+
+	use Rack::Session::Cookie, secret: "ome_nice_long_random_string_DSKJH4378EYR7EGKUFH", key: "_myapp_session"
+	use Rack::Protection
+	plugin :csrf
+
+	require './models/user.rb'
+
 	plugin :static, ["/images", "/css", "/js"]
 	plugin :render
 	plugin :head
@@ -24,5 +34,51 @@ class Myapp < Roda
 		r.get "contact" do
 			view("contact")
 		end
+
+		r.get "login" do
+      view("login")
+    end
+
+    r.post "login" do
+      if user = User.authenticate(r["email"], r["password"])
+        session[:user_id] = user.id
+        r.redirect "/"
+      else
+        r.redirect "/login"
+      end
+    end
+
+    r.post "logout" do
+      session.clear
+      r.redirect "/"
+    end
+
+    r.on "users" do
+    	r.get "new" do
+        @user = User.new
+        view("users/new")
+      end
+
+      r.get ":id" do |id|
+        @user = User[id]
+        view("users/show")
+      end
+
+      r.is do
+        r.get do
+          @users = User.order(:id)
+          view("users/index")
+        end
+
+        r.post do
+          @user = User.new(r["user"])
+          if @user.valid? && @user.save
+            r.redirect "/users"
+          else
+            view("users/new")
+          end
+        end
+      end
+    end
 	end
 end
